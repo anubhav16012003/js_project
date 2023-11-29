f19fd0798d725375ae76bfbab00b74cbc4de5b7c
Anubhav Verma <98375739+anubhav16012003@users.noreply.github.com>
2023-11-29 20:19:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const body = document.querySelector('body');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(function(button){
+    console.log(button);
+    button.addEventListener('click', function(e){
+        console.log(e);
+        console.log(e.target);
+        if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'red'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'grey'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'purple'){
+            body.style.backgroundColor = e.target.id;
+        }
+    })
+});
